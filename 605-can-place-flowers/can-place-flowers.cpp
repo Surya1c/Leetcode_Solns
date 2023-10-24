ed450bee170df49c73213a9673ab83b526599025
Surya1c <107142266+Surya1c@users.noreply.github.com>
2023-10-24 22:19:53 +0530

    Time: 15 ms (44.36%) | Memory: 20.6 MB (30.07%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,17 +1,14 @@
+
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-   int total=0;
-   for(int i=0;i<flowerbed.size() && total <n;i++){
-       if(flowerbed[i]==0){
-           int next= (i==flowerbed.size()-1)?0:flowerbed[i+1];
-           int prev=(i==0)? 0: flowerbed[i-1];
-           if(next==0&& prev==0){
-               flowerbed[i]=1;
-               total++;
-           }
-       }
-   }
-   return total==n;
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+        return count >= n;
     }
 };
