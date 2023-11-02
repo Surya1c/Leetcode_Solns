33fe371b8d43c9a3c9a96277a4e79bec10175cb9
Surya1c <107142266+Surya1c@users.noreply.github.com>
2023-11-02 13:15:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (71.89%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -9,27 +9,26 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+typedef pair<int,int> ppi;
 class Solution {
 public:
-    int averageOfSubtree(TreeNode* root) {
-        int result = 0;
-        traverse(root, result);
-        return result;
-    }
+	int count=0;
+	ppi f(TreeNode* root)
+	{
+		if(root == NULL) return {0,0};                  // base case
+		ppi l = f(root->left);
+		ppi r = f(root->right);
+		int lsum = l.first;                             // sum of node values in left subtree
+		int rsum = r.first;                             // sum of node values in right subtree
+		int lcount = l.second;                          // number of nodes in left subtree
+		int rcount = r.second;                          // number of nodes in right subtree
 
-private:
-    pair<int, int> traverse(TreeNode* node, int& result) {
-        if (!node) return {0, 0};
-        
-        auto [left_sum, left_count] = traverse(node->left, result);
-        auto [right_sum, right_count] = traverse(node->right, result);
-        
-        int curr_sum = node->val + left_sum + right_sum;
-        int curr_count = 1 + left_count + right_count;
-        
-        if (curr_sum / curr_count == node->val) result++;
-        
-        return {curr_sum, curr_count};
-    }
-
+		int sum = (lsum + rsum + root->val)/(lcount+rcount+1);  // calculating average
+		if(sum == root->val)count++;                           
+		return {lsum+rsum+root->val,lcount+rcount+1};           
+	}
+	int averageOfSubtree(TreeNode* root) {
+		f(root);
+		return count;                                 
+	}
 };
