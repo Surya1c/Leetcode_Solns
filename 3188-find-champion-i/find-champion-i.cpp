79e5b5f223d8cd55b4bae7010f86cc6ef026c5c3
Surya1c <107142266+Surya1c@users.noreply.github.com>
2023-11-09 15:09:17 +0530

    Time: 47 ms (97.74%) | Memory: 36.7 MB (48.78%) - LeetSync

--- 3188-find-champion-i/find-champion-i.cpp
+++ 3188-find-champion-i/find-champion-i.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public:   
-int findChampion(vector<vector<int>>& grid) {
-    int ans = -1, count = 0;
-    vector<int> indegree(grid.size(), 0);
-    for(int i = 0; i < grid.size(); ++i){
-        for(int j = 0; j < grid.size(); ++j){
-            if(i == j) continue;
-            if(grid[i][j]) indegree[j]++;
-            else indegree[i]++;
-        }
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int store = 0;
+        int maxi = INT_MIN;
+       for(int i = 0; i < grid.size(); i++){
+           int count = 0;
+           for(int j = 0; j <grid[i].size(); j++){
+               if(grid[i][j] == 1){
+                   count++;
+               }
+           }
+           if(count > maxi){
+               maxi = count;
+               store = i;
+           }
+       }
+       return store;
     }
-    for(int i = 0; i < grid.size(); ++i) {
-        if(indegree[i] == 0) {count++; ans = i; }
-    }
-    return count > 1?-1:ans;
-}
 };
